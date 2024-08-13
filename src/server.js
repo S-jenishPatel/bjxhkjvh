79f5899a67d4e72d258d699861558e00fad8a47e
Jenish Patel Sanandiya <sjenishpatel23@gmail.com>
2024-08-13 09:23:54 +0530

    add console.log

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
+const { app } = require("./app.js");
 
-const { app } = require('./app.js');
-
-const port=process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log("Server is running at port :", port);
-})
+});
+
+console.log("hello");
